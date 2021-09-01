c0e4db4612d1b502886cd3e8bc54cdbbda969438
James Wasssinger <jameswassinger@users.noreply.github.com>
2021-09-01 14:18:29 -0500

    Add files via upload

---
+++ SCRIPTS/Invoke-PSTemplate.ps1
@@ -0,0 +1,36 @@
+﻿[CmdletBinding()]
+param(
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [System.IO.FileInfo]$Log
+
+)
+
+if(Test-Path -Path $Log) {
+    Remove-Item -Path $Log -Force
+}
+
+& {
+
+    Write-Host "==================== START $(Get-Date) ===================="
+
+   try {
+
+
+
+
+
+
+   } catch {
+
+
+
+
+
+   }
+
+   Write-Host "==================== END $(Get-Date) ===================="
+
+
+} *>> $Log
